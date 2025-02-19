b69184561cb72c55d83d5fe481bc752a502eeeef
ITsAYanic <itsayanic@gmail.com>
2025-02-19 10:27:54 +0100

    added the code that already existed

---
+++ CsharpLearning/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CsharpLearning;
+
+class Program
+{
+    static public int DefineInteacher = -1;
+    static public double DefineDouble = 1.5555;
+    static private char DefineChar = ' ';
+    static private string DefineString = "ist das Ergebnis!";
+    static private char GetCharFromString = DefineString[2];
+    static public double AddAIntAndADouble = DefineInteacher + DefineDouble;
+    static public double SubtractADoubleFromInt = DefineDouble - 0.5555;
+    static public double MuliplyTwoNumbers = SubtractADoubleFromInt * DefineInteacher;
+    static private int ConvertCharToInt = (int)DefineChar;
+    static private char ConvertIntToChar = (char)50;
+    static string sentence = DefineInteacher.ToString();
+
+    
+
+    
+    static void Main(string[] args)
+    {
+
+    }
+
+};
---
+++ thermometer/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace thermometer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rd = new Random();
+        int temparature = rd.Next(-100, 100);
+        if (temparature > 0 && temparature <= 30){
+            Console.Write("It's not Freezing with a Temparature of " + temparature + "°C");
+        }
+        else if (temparature > 30) {
+            Console.Write("It's boiling hot with a temparature of " + temparature + "°C");
+        }
+        else if (temparature < 0) {
+            Console.Write("It's Freezing with a Temparature of " + temparature + "°C");
+        }
+        else if (temparature == 0) {
+            Console.Write("It's " + temparature + " Degrees");
+        }
+    }
+}
