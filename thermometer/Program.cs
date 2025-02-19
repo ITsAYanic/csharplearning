9d03cd1b418e7f5009c5796be339ee1503bedc62
ITsAYanic <itsayanic@gmail.com>
2025-02-19 11:02:18 +0100

    The thermometer should work now perfectly fine, alteast I hope so

--- thermometer/Program.cs
+++ thermometer/Program.cs
@@ -5,18 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Random rd = new Random();
-        int temparature = rd.Next(-100, 100);
-        if (temparature > 0 && temparature <= 30){
-            Console.Write("It's not Freezing with a Temparature of " + temparature + "°C");
+        int temperature = rd.Next(-100, 100);
+        if (temperature > 0 && temperature < 31){
+            Console.Write("It's not Freezing with a Temparature of " + temperature + "°C");
         }
-        else if (temparature > 30) {
-            Console.Write("It's boiling hot with a temparature of " + temparature + "°C");
+        else if (temperature > 30) {
+            Console.Write("It's boiling hot with a temparature of " + temperature + "°C");
         }
-        else if (temparature < 0) {
-            Console.Write("It's Freezing with a Temparature of " + temparature + "°C");
+        else if (temperature < 0) {
+            Console.Write("It's Freezing with a Temparature of " + temperature + "°C");
         }
-        else if (temparature == 0) {
-            Console.Write("It's " + temparature + " Degrees");
+        else if (temperature == 0) {
+            Console.Write("It's " + temperature + " Degrees");
+        }
+        else
+        {
+            Console.Write("Wait, I didn't intend for it to show this message? This is the Value where it went wrong:" + temperature);
         }
     }
 }
--- thermometer/obj/Debug/net8.0/thermometer.AssemblyInfo.cs
+++ thermometer/obj/Debug/net8.0/thermometer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("thermometer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3af0d628e0cdb6c1dc31c0f8c36f69b2154f5caa")]
 [assembly: System.Reflection.AssemblyProductAttribute("thermometer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("thermometer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
